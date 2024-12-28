8c466d8aa4287fd9c9eb69b5aa4b8e13f1e64135
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2024-12-28 18:16:28 +0300

    Değişiklikler

---
+++ NTierArchitectureBtk/Controllers/BooksController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NTierArchitectureBtk.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+    }
+}
