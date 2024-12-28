2b36d3205d86cce9480a602527c85cc7557e99d5
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2024-12-28 18:21:30 +0300

    temizlendi

--- RepositoryLayer/Constract/IRepositoryBase.cs
+++ RepositoryLayer/Constract/IRepositoryBase.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace RepositoryLayer.Constract
 {
-    public interface IRepositoryBase
+    public interface IRepositoryBase<T>
     {
-
+        IQueryable<T> FindAll(bool trackChanges);
+        IQueryable FindByCondition(Expression<Func<T, bool>> expressinon, bool condition);
+        void Create(T entity);
+        void Update(T entity);
+        void Delete(T entity);
     }
 }
---
+++ RepositoryLayer/EfCore/RepositoryContext.cs
@@ -0,0 +1,17 @@
+﻿using Entity.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryLayer.EfCore
+{
+    public class RepositoryContext:DbContext
+    {
+        
+        public RepositoryContext(DbContextOptions options):base(options) { }
+        public DbSet<Book> Books { get; set; }
+    }
+}
